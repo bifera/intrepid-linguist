208b84d64184cdd6fa0b3d9e89e46a76ab43b957
Justyna Rudnicka-Staniewska <jete.justyce@gmail.com>
2017-12-16 15:50:46 +0100

    update theme

---
+++ wp-content/themes/jedrzejowski-theme/gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+
+var input = './library/scss/style.scss';
+
+var sassOptions = {
+    errLogToConsole: true,
+    outputStyle: 'nested',
+    sourceComments: 'map'
+};
+
+var sassProdOptions = {
+    errLogToConsole: true,
+    outputStyle: 'compressed'
+}
+
+gulp.task('checkJs', function(){
+    return gulp.src('./coffee-wp/wp-content/themes/anfrawer/js/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('sass', function(){
+    return gulp.src(input)
+    .pipe(sourcemaps.init())
+    .pipe(sass(sassOptions))
+    .pipe(sourcemaps.write())
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('./library/css'))
+});
+
+gulp.task('sass-prod', function(){
+    return gulp.src(input)
+    .pipe(sass(sassProdOptions))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest(''))
+});
+
+gulp.task('watch', function(){
+    gulp.watch('scss/**/*.scss', ['sass']);
+});
